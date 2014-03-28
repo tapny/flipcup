62c2348f1af9fa2c155b97eafd9aa3cae8c5cbd0
victor <vjwang45@gmail.com>
2014-03-28 13:01:50 -0400

    index js

---
+++ index.js
@@ -0,0 +1,45 @@
+
+
+$(document).ready(function() {
+    
+    var roundNumber = 1;
+
+    var makeTeamElem = function(text) {
+        return "<div class='team'>" + text + "</div>";
+    };
+
+    var makeCheckboxElem = function() {
+        return "<input type='checkbox' value='winner'>   Winner?"
+    };
+    $('.enter-team').keyup( function(e) {
+        var that = $(this);
+        if (e.keyCode == 13) {
+            // add to teams section
+            $('.team-names').append( makeTeamElem(that.val()));
+
+            // clear textbox
+            that.val("");
+        }
+    });
+
+    $('.start').click( function(e) {
+        // read all teams
+        var teams = $('.team')
+
+        // randomize
+        teams.sort(function() {
+            return Math.random() - 0.5;
+        });
+        // display
+        $.each(teams, function(index, elem) {
+            var item = $(elem)
+            var extra = "<br>"
+            if (index %2 == 0) {
+                // odds 
+               extra = "vs <br>" 
+            }
+            
+            $('.round1').append(makeTeamElem(item.text()) + makeCheckboxElem() + extra);
+        });
+    });
+});
