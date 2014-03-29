727fea029f81d144b19b6f66179d9df5980bee14
Victor J Wang <vjwang45@gmail.com>
2014-03-28 20:26:26 -0400

    working winner advancement, add jquery min file

--- index.js
+++ index.js
@@ -5,11 +5,11 @@ $(document).ready(function() {
     var roundNumber = 1;
 
     var makeTeamElem = function(text) {
-        return "<div class='team'>" + text + "</div>";
+        return "<div class='team' value='" + text + "'>" + text + "</div>";
     };
 
-    var makeCheckboxElem = function() {
-        return "<input type='checkbox' value='winner'>   Winner?"
+    var makeCheckboxElem = function(text) {
+        return "<input type='checkbox' value='" + text + "'/>"
     };
     $('.enter-team').keyup( function(e) {
         var that = $(this);
@@ -22,15 +22,18 @@ $(document).ready(function() {
         }
     });
 
-    $('.start').click( function(e) {
-        // read all teams
-        var teams = $('.team')
+    var randomizeAndDisplay = function(teams, round) {
+
+        if (teams.length == 1)
+            return
 
         // randomize
         teams.sort(function() {
             return Math.random() - 0.5;
         });
+
         // display
+        var roundSection = $("<div class=round" +round + "></div>");
         $.each(teams, function(index, elem) {
             var item = $(elem)
             var extra = "<br>"
@@ -38,10 +41,31 @@ $(document).ready(function() {
                 // odds 
                extra = "vs <br>" 
             }
-            // TODO variablize round number 
-            $('.round1').append(makeTeamElem(item.text()) + makeCheckboxElem() + extra);
-        });
 
-        $('.round1').append("<button>Advance Winners</button>");
+            // TODO variablize round number 
+            var teamName = item.attr("value");
+            var teamElem = $(makeTeamElem(teamName));
+            teamElem.prepend(makeCheckboxElem(teamName));
+
+            roundSection.append(teamElem.get(0));
+            roundSection.append(extra);
+        });
+        
+        // winners button, attach click action
+        var winnerButton = $("<button>Advance Winners</button>");
+        winnerButton.click( function() {
+            var winners = $(".round" + round + " input:checked");
+            randomizeAndDisplay(winners, round+1);
+        });
+        roundSection.append(winnerButton);
+        
+        // add finished section
+        $('.rounds').append(roundSection)
+    };
+
+    $('.start').click( function(e) {
+        // read all teams
+        var teams = $('.team');
+        randomizeAndDisplay(teams, 1);
     });
 });
